d6c4829f7f8be53c8f4eef1ffe6da96c9ee86a54
oldDong <2232890213@qq.com>
2020-10-25 20:45:32 +0800

    +1的案例

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-const App=()=>{
-    return(
-        <div>App组件</div>
-    );
-}
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.querySelector("#root");
+let n = 0;
+const App = () =>
+  React.createElement("div", { className: "red" }, [
+    n,
+    React.createElement(
+      "button",
+      {
+        onClick: () => {
+          n += 1;
+          ReactDOM.render(App(), root);
+        }
+      },
+      "+1"
+    )
+  ]);
 
+ReactDOM.render(App(), root);
