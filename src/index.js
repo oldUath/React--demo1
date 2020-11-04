ace71f08c6145ede5928ead941549a2d2432ca18
oldUath <haiyangdong666@gmail.com>
2020-11-04 23:45:10 +0800

    class组件和function组件

--- src/index.js
+++ src/index.js
@@ -1,6 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './style.css';
+
+function App(){
+    return(
+        <div className="App">
+            爸爸
+            <Son />
+        </div>
+    )
+};
+
+// 类组件中都以方法
+class Son extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            n:0
+        };
+    };
+    add(){
+        // this.state.n+=1
+        this.setState({ n: this.state.n + 1 });
+
+    };
+    render(){
+        return(
+            <div className="Son">
+               儿子 n:{this.state.n}
+               <button onClick={()=>this.add()}>+1</button>
+               <Grandson />
+            </div>
+        )
+    }
+
+};
+
+const Grandson = ()=>{
+    const [n,setN] = React.useState(0)
+    return(
+        <div className="Grandson">
+            孙子 n:{n}
+            <button onClick={()=>setN(n+1)}>+1</button>
+        </div>
+    )
+}
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
